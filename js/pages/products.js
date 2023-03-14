a39ef4a5dcd05bbf5d5119fb3b174af85ec6f5dc
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 14:54:39 +0700

    page loading

--- js/pages/products.js
+++ js/pages/products.js
@@ -4,4 +4,6 @@ import '../cart/toggleCart.js';
 import display from '../displayProducts.js';
 import { store } from '../store.js'
 import { getElement } from '../utils.js';
-display(store, getElement('.products-container'))
+display(store, getElement('.products-container'));
+const loading = getElement('.page-loading');
+loading.style.display = 'none';
