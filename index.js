efb2a7b0eb746e8cce5c0886625b8841ff6d1b3f
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 14:01:46 +0700

    fetch products

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import './js/toggleSidebar.js';
 import './js/cart/toggleCart.js';
+import fetchProducts from './js/fetchProducts.js';
+
+const init = async () => {
+    const products = await fetchProducts();
+    console.log(products);
+}
+
+window.addEventListener('DOMContentLoaded', init)
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,17 @@
-export const getElement = (selection) => {
+const allProductsUrl = 'https://course-api.com/javascript-store-products'
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl =
+    'https://course-api.com/javascript-store-single-product'
+
+
+const getElement = (selection) => {
     const element = document.querySelector(selection)
     if (element) return element
     throw new Error(`Please check "${selection}" selector, no such element exist`)
+}
+export {
+    allProductsUrl,
+    singleProductUrl,
+    getElement,
 }
