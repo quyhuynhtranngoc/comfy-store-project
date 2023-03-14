c11810dbf41ec4c2a81fc383ba83f52cc6f4eae0
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 18:43:17 +0700

    fetch single product

--- js/pages/product.js
+++ js/pages/product.js
@@ -1,2 +1,14 @@
 import '../toggleSidebar.js';
 import '../cart/toggleCart.js';
+import { getElement, singleProductUrl } from '../utils.js';
+
+// selections
+const loading = getElement('.page-loading');
+
+// show product when page loads
+window.addEventListener('DOMContentLoaded', async function () {
+    const urlID = window.location.search;
+    const response = await fetch(`${singleProductUrl}${urlID}`);
+    console.log(response);
+    loading.style.display = 'none';
+});
