0a84393818ee4b9898c93419bc5fbbcf3a17933c
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 14:09:28 +0700

    setup store

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import './js/toggleSidebar.js';
 import './js/cart/toggleCart.js';
 import fetchProducts from './js/fetchProducts.js';
+import { setupStore, store } from './js/store.js';
 
 const init = async () => {
     const products = await fetchProducts();
-    console.log(products);
-}
+    if (products) {
+        // add products to the store
+        setupStore(products);
+        console.log(store);
+    }
+};
 
 window.addEventListener('DOMContentLoaded', init)
--- js/utils.js
+++ js/utils.js
@@ -10,8 +10,26 @@ const getElement = (selection) => {
     if (element) return element
     throw new Error(`Please check "${selection}" selector, no such element exist`)
 }
+
+
+const getStorageItem = (item) => {
+    let storageItem = localStorage.getItem(item)
+    if (storageItem) {
+        storageItem = JSON.parse(localStorage.getItem(item))
+    } else {
+        storageItem = []
+    }
+    return storageItem
+}
+
+const setStorageItem = (name, item) => {
+    localStorage.setItem(name, JSON.stringify(item))
+}
+
 export {
     allProductsUrl,
     singleProductUrl,
     getElement,
+    getStorageItem,
+    setStorageItem
 }
