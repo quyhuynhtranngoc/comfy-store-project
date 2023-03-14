07b04acddf0710c58dbe020b33aa26f3a9ccfb96
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 15:19:23 +0700

    search filter

---
+++ js/filters/search.js
@@ -0,0 +1,31 @@
+import display from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+const setupSearch = store => {
+    const inputForm = getElement('.input-form');
+    const searchInput = getElement('.search-input');
+
+    inputForm.addEventListener('keyup', function () {
+        const value = searchInput.value;
+        if (value) {
+            const newStore = store.filter(product => {
+                let { name } = product;
+                name = name.toLowerCase();
+                if (name.startsWith(value)) {
+                    return product;
+                }
+            })
+            display(newStore, getElement('.products-container'));
+            if (newStore.length < 1) {
+                const products = getElement('.products-container');
+                products.innerHTML = `<h3 class="filter-error">sorry, no products matched your search</h3>`
+            }
+        }
+        else {
+            display(store, getElement('.products-container'));
+        }
+    })
+
+}
+
+export default setupSearch;
--- js/pages/products.js
+++ js/pages/products.js
@@ -4,6 +4,9 @@ import '../cart/toggleCart.js';
 import display from '../displayProducts.js';
 import { store } from '../store.js'
 import { getElement } from '../utils.js';
+import setupSearch from '../filters/search.js';
 display(store, getElement('.products-container'));
 const loading = getElement('.page-loading');
 loading.style.display = 'none';
+
+setupSearch(store);
