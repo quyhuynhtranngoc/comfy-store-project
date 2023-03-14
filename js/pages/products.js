e2db020443cd918b0fef6577dc26c241baa3e190
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 18:10:58 +0700

    companies filter

--- js/pages/products.js
+++ js/pages/products.js
@@ -5,8 +5,11 @@ import display from '../displayProducts.js';
 import { store } from '../store.js'
 import { getElement } from '../utils.js';
 import setupSearch from '../filters/search.js';
+import setupCompanies from '../filters/companies.js';
+
 display(store, getElement('.products-container'));
 const loading = getElement('.page-loading');
 loading.style.display = 'none';
 
 setupSearch(store);
+setupCompanies(store);
