6e73ed4f6b837723aa3a33b9890e571a26149b95
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 14:23:10 +0700

    display products

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import './js/toggleSidebar.js';
 import './js/cart/toggleCart.js';
 import fetchProducts from './js/fetchProducts.js';
 import { setupStore, store } from './js/store.js';
+import display from './js/displayProducts.js';
+import { getElement } from './js/utils.js';
 
 const init = async () => {
     const products = await fetchProducts();
     if (products) {
         // add products to the store
         setupStore(products);
-        console.log(store);
+        const featured = store.filter((product) => product.featured === true);
+        display(featured, getElement('.featured-center'));
     }
 };
 
