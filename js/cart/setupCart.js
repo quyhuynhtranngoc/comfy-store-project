23b6b317ec746f6ec8208cc6c0d4766fe8cccc71
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-14 21:09:04 +0700

    addtocart dom

--- js/cart/setupCart.js
+++ js/cart/setupCart.js
@@ -1,6 +1,27 @@
+import { findProduct } from "../store.js";
+import { getStorageItem } from "../utils.js";
+import addToCartDOM from "./addToCartDOM.js";
 import { openCart } from "./toggleCart.js";
 
+let cart = getStorageItem('cart');
+
 export const addToCart = (id) => {
-    console.log(id);
+    let item = cart.find(cartItem => cartItem.id === id);
+    if (!item) {
+        let product = findProduct(id);
+        product = { ...product, amount: 1 };
+        cart = [...cart, product];
+        addToCartDOM(product);
+        console.log(cart);
+    } else {
+        // update values
+    }
+
     openCart();
 }
+
+const init = () => {
+    console.log(cart);
+}
+
+init();
--- js/store.js
+++ js/store.js
@@ -13,4 +13,9 @@ const setupStore = (products) => {
     setStorageItem('store', store);
 };
 
-export { store, setupStore };
+const findProduct = id => {
+    let product = store.find(product => product.id === id);
+    return product;
+}
+
+export { store, setupStore, findProduct };
